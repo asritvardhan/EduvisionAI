a8d3f5688637828242ccd50eb6f19a507cacd162
ASRIT VARDHAN GOLI <154015063+asritvardhan@users.noreply.github.com>
2026-03-05 13:17:33 +0530

    Change app run port to use environment variable

--- backend/main.py
+++ backend/main.py
@@ -1537,4 +1537,5 @@ def serve_frontend(path):
 # Run App
 # ------------------------- #
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
